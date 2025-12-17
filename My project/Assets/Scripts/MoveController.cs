04d6913a6e7a2598fbf1134ab709135fd0a213f7
LlucVelazquez <lluc.velazquez.7e8@itb.cat>
2025-12-17 20:00:41 +0100

    Move Controller started

--- My project/Assets/Scripts/MoveController.cs
+++ My project/Assets/Scripts/MoveController.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float velocity = 5f;
+    private CharacterController characterController;
+    private InputSystem_Actions controller;
+    private Vector2 inputMove;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        controller = new InputSystem_Actions();
+        controller.Player.Move.performed += context => inputMove = context.ReadValue<Vector2>();
+        controller.Player.Move.canceled += context => inputMove = Vector2.zero;
+    }
+    private void OnEnable()
+    {
+        controller.Enable();
+    }
+    private void OnDisable()
+    {
+        controller.Disable();
+    }
+    private void Update()
+    {
+        Vector3 move = transform.right * inputMove.x + transform.forward * inputMove.y;
+        characterController.Move(move * velocity * Time.deltaTime);
     }
 }
